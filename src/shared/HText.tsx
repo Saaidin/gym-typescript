ebd306ae607ac8c1a4976e2a14464cf89038f0c1
saaidin <dinme73@gmail.com>
2023-06-17 15:06:22 +0800

    Benefits section - done

--- src/shared/HText.tsx
+++ src/shared/HText.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const HText = () => {
-  return <div>HText</div>;
+type Props = {
+  children: React.ReactNode;
+};
+
+const HText = ({ children }: Props) => {
+  return (
+    <h1 className="basis-3/5 font-montserrat text-3xl font-bold">{children}</h1>
+  );
 };
 
 export default HText;
